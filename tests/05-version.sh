98930afc14c617ac9ef84858c68da2aa809c3fe1
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2025-01-05 03:13:41 -0500

    reorder

--- tests/05-version.sh
+++ tests/05-version.sh
@@ -6,10 +6,10 @@ WHEREAMI=$(dirname "$(realpath "$0")")
 . $WHEREAMI/setup.sh
 
 test_expect_success 'cabin version' '
-    "$CABIN_BIN" version 1>actual &&
     VERSION=$(grep -m1 version "$WHEREAMI"/../cabin.toml | cut -f 2 -d'\''"'\'') &&
     COMMIT_SHORT_HASH=$(git rev-parse --short HEAD) &&
     COMMIT_DATE=$(git show -s --date=format-local:'\''%Y-%m-%d'\'' --format=%cd) &&
+    "$CABIN_BIN" version 1>actual &&
     cat >expected <<-EOF &&
 cabin $VERSION ($COMMIT_SHORT_HASH $COMMIT_DATE)
 EOF
